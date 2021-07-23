8754d70ca6523a0647be20a6eac63d928f3575a2
Ara <60602858+arajo-hub@users.noreply.github.com>
2021-07-23 16:26:24 +0900

    Vol.1 P.278 <3장. 템플릿> 끝

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -1,11 +1,17 @@
 package springbook.user.dao;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.List;
 
 /**
  * 1.1.2 UserDao
@@ -539,64 +545,144 @@ import java.sql.*;
 /**
  * JdbcContext를 DI 받아서 사용하도록 만든 UserDao
  */
+//public class UserDao {
+//
+//    private DataSource dataSource;
+//    private JdbcContext jdbcContext;
+//
+//    // JdbcContext 생성과 DI 작업을 수행
+//    public void setDataSource(DataSource dataSource) {
+//        this.jdbcContext = new JdbcContext();
+//        this.jdbcContext.setDataSource(dataSource);
+//        this.dataSource = dataSource;
+//    }
+//
+////    // JdbcContext DI
+////    public void setJdbcContext(JdbcContext jdbcContext) {
+////        this.jdbcContext = jdbcContext;
+////    }
+//
+//    public void add(final User user) throws SQLException {
+//        this.jdbcContext.workWithStatementStrategy(
+//            new StatementStrategy() {
+//                @Override
+//                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+//                    PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+//                    ps.setString(1, user.getId());
+//                    ps.setString(2, user.getName());
+//                    ps.setString(3, user.getPassword());
+//
+//                    return ps;
+//                }
+//            }
+//        );
+//    }
+//
+////    public void deleteAll() throws SQLException {
+////        this.jdbcContext.workWithStatementStrategy(
+////            new StatementStrategy() {
+////                @Override
+////                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+////                    return c.prepareStatement("delete from users");
+////                }
+////            }
+////        );
+////    }
+//
+//    public void deleteAll() throws SQLException {
+//        this.jdbcContext.executeSql("delete from users");
+//    }
+//
+//     // 아래 메소드만 JdbcContext로 옮긴다.
+////    private void executeSql(final String query) throws SQLException {
+////        this.jdbcContext.workWithStatementStrategy(
+////            new StatementStrategy() {
+////                @Override
+////                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+////                    return c.prepareStatement(query);
+////                }
+////            }
+////        );
+////    }
+//
+//}
+
+/**
+ * 스프링의 JdbcTemplate을 사용
+ */
 public class UserDao {
 
-    private DataSource dataSource;
-    private JdbcContext jdbcContext;
-
-    // JdbcContext 생성과 DI 작업을 수행
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcContext = new JdbcContext();
-        this.jdbcContext.setDataSource(dataSource);
-        this.dataSource = dataSource;
-    }
-
-//    // JdbcContext DI
-//    public void setJdbcContext(JdbcContext jdbcContext) {
-//        this.jdbcContext = jdbcContext;
-//    }
-
-    public void add(final User user) throws SQLException {
-        this.jdbcContext.workWithStatementStrategy(
-            new StatementStrategy() {
-                @Override
-                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                    PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-                    ps.setString(1, user.getId());
-                    ps.setString(2, user.getName());
-                    ps.setString(3, user.getPassword());
-
-                    return ps;
+//    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
+    private RowMapper<User> userMapper =
+            new RowMapper<User>() {
+                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    User user = new User();
+                    user.setId(rs.getString("id"));
+                    user.setName(rs.getString("name"));
+                    user.setPassword(rs.getString("password"));
+                    return user;
                 }
-            }
-        );
+            };
+
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+//        this.dataSource = dataSource;
     }
 
-//    public void deleteAll() throws SQLException {
-//        this.jdbcContext.workWithStatementStrategy(
-//            new StatementStrategy() {
-//                @Override
-//                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-//                    return c.prepareStatement("delete from users");
-//                }
-//            }
-//        );
-//    }
-
-    public void deleteAll() throws SQLException {
-        this.jdbcContext.executeSql("delete from users");
+    public void add(final User user) {
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword());
     }
 
-     // 아래 메소드만 JdbcContext로 옮긴다.
-//    private void executeSql(final String query) throws SQLException {
-//        this.jdbcContext.workWithStatementStrategy(
-//            new StatementStrategy() {
-//                @Override
-//                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-//                    return c.prepareStatement(query);
+    public User get(String id) {
+        return this.jdbcTemplate.queryForObject("select * from users where id =?",
+            new Object[] {id}, this.userMapper
+//            new RowMapper<User>() {
+//                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                    User user = new User();
+//                    user.setId(rs.getString("id"));
+//                    user.setName(rs.getString("name"));
+//                    user.setPassword(rs.getString("password"));
+//                    return user;
 //                }
 //            }
-//        );
-//    }
+            );
+    }
+
+    public void deleteAll() {
+        this.jdbcTemplate.update("delete from users");
+    }
+
+    public int getCount() {
+//        return this.jdbcTemplate.query(new PreparedStatementCreator() {
+//            @Override
+//            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+//                return con.prepareStatement("select count(*) from users");
+//            }
+//        }, new ResultSetExtractor<Integer>() {
+//            @Override
+//            public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+//                rs.next();
+//                return rs.getInt(1);
+//            }
+//        });
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+    }
+
+    public List<User> getAll() {
+        return this.jdbcTemplate.query("select * from users order by id",
+            this.userMapper
+//            new RowMapper<User>() {
+//                public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                    User user = new User();
+//                    user.setId(rs.getString("id"));
+//                    user.setName(rs.getString("name"));
+//                    user.setPassword(rs.getString("password"));
+//                    return user;
+//                }
+//            }
+            );
+    }
 
 }
--- src/test/java/springbook/learningtest/template/Calculator.java
+++ src/test/java/springbook/learningtest/template/Calculator.java
@@ -1,143 +1,143 @@
-package springbook.learningtest.template;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-public class Calculator {
-
-    public Integer calcSum(String filepath) throws IOException {
-//        BufferedReader br = new BufferedReader(new FileReader(filepath));
-//        Integer sum = 0;
-//        String line = null;
-//        while ((line = br.readLine()) != null) {
-//            sum += Integer.valueOf(line);
-//        }
+//package springbook.learningtest.template;
 //
-//        br.close();
-//        return sum;
-
-        // try/catch/finally 적용
+//import java.io.BufferedReader;
+//import java.io.FileReader;
+//import java.io.IOException;
+//
+//public class Calculator {
+//
+//    public Integer calcSum(String filepath) throws IOException {
+////        BufferedReader br = new BufferedReader(new FileReader(filepath));
+////        Integer sum = 0;
+////        String line = null;
+////        while ((line = br.readLine()) != null) {
+////            sum += Integer.valueOf(line);
+////        }
+////
+////        br.close();
+////        return sum;
+//
+//        // try/catch/finally 적용
+////        BufferedReader br = null;
+////
+////        try {
+////            br = new BufferedReader(new FileReader(filepath));
+////            Integer sum = 0;
+////            String line = null;
+////            while ((line = br.readLine()) != null) {
+////                sum += Integer.valueOf(line);
+////            }
+////            return sum;
+////        } catch (IOException e) {
+////            System.out.println(e.getMessage());
+////            throw e;
+////        } finally {
+////            if (br != null) {
+////                try {
+////                    br.close();
+////                } catch (IOException e) {
+////                    System.out.println(e.getMessage());
+////                }
+////            }
+////
+////        }
+//
+//        // 템플릿/콜백을 적용한 calcSum() 메소드
+////        BufferedReaderCallback sumCallback =
+////            new BufferedReaderCallback() {
+////                @Override
+////                public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+////                    Integer sum = 0;
+////                    String line = null;
+////                    while ((line = br.readLine()) != null) {
+////                        sum += Integer.valueOf(line);
+////                    }
+////                    return sum;
+////                }
+////            };
+////
+////        return fileReadTemplate(filepath, sumCallback);
+//
+//        // lineReadTemplate()을 사용하도록 수정
+//        LineCallback sumCallback =
+//            new LineCallback() {
+//            public Integer doSomethingWithLine(String line, Integer value) {
+//                return value + Integer.valueOf(line);
+//            }};
+//        return lineReadTemplate(filepath, sumCallback, 0);
+//    }
+//
+//    public Integer calcMultiply(String filepath) throws IOException {
+////        BufferedReaderCallback multiplyCallback =
+////                new BufferedReaderCallback() {
+////                    @Override
+////                    public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+////                        Integer multiply = 1;
+////                        String line = null;
+////                        while ((line = br.readLine()) != null) {
+////                            multiply *= Integer.valueOf(line);
+////                        }
+////                        return multiply;
+////                    }
+////                };
+////        return fileReadTemplate(filepath, multiplyCallback);
+//
+//        // lineReadTemplate()을 사용하도록 수정
+//        LineCallback multiplyCallback =
+//            new LineCallback() {
+//                @Override
+//                public Integer doSomethingWithLine(String line, Integer value) {
+//                    return value * Integer.valueOf(line);
+//                }};
+//            return lineReadTemplate(filepath, multiplyCallback, 1);
+//    }
+//
+//    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
 //        BufferedReader br = null;
-//
 //        try {
 //            br = new BufferedReader(new FileReader(filepath));
-//            Integer sum = 0;
-//            String line = null;
-//            while ((line = br.readLine()) != null) {
-//                sum += Integer.valueOf(line);
-//            }
-//            return sum;
+//            int ret = callback.doSomethingWithReader(br);
+//            return ret;
 //        } catch (IOException e) {
 //            System.out.println(e.getMessage());
 //            throw e;
 //        } finally {
 //            if (br != null) {
-//                try {
-//                    br.close();
-//                } catch (IOException e) {
-//                    System.out.println(e.getMessage());
-//                }
+//                try { br.close(); }
+//                catch (IOException e) { System.out.println(e.getMessage()); }
 //            }
-//
 //        }
-
-        // 템플릿/콜백을 적용한 calcSum() 메소드
-//        BufferedReaderCallback sumCallback =
-//            new BufferedReaderCallback() {
-//                @Override
-//                public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-//                    Integer sum = 0;
-//                    String line = null;
-//                    while ((line = br.readLine()) != null) {
-//                        sum += Integer.valueOf(line);
-//                    }
-//                    return sum;
-//                }
-//            };
+//    }
 //
-//        return fileReadTemplate(filepath, sumCallback);
-
-        // lineReadTemplate()을 사용하도록 수정
-        LineCallback sumCallback =
-            new LineCallback() {
-                public Integer doSomethingWithLine(String line, Integer value) {
-                    return value + Integer.valueOf(line);
-                }};
-        return lineReadTemplate(filepath, sumCallback, 0);
-    }
-
-    public Integer calcMultiply(String filepath) throws IOException {
-//        BufferedReaderCallback multiplyCallback =
-//                new BufferedReaderCallback() {
-//                    @Override
-//                    public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-//                        Integer multiply = 1;
-//                        String line = null;
-//                        while ((line = br.readLine()) != null) {
-//                            multiply *= Integer.valueOf(line);
-//                        }
-//                        return multiply;
-//                    }
-//                };
-//        return fileReadTemplate(filepath, multiplyCallback);
-
-        // lineReadTemplate()을 사용하도록 수정
-        LineCallback multiplyCallback =
-            new LineCallback() {
-                @Override
-                public Integer doSomethingWithLine(String line, Integer value) {
-                    return value * Integer.valueOf(line);
-                }};
-            return lineReadTemplate(filepath, multiplyCallback, 1);
-    }
-
-    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(filepath));
-            int ret = callback.doSomethingWithReader(br);
-            return ret;
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            throw e;
-        } finally {
-            if (br != null) {
-                try { br.close(); }
-                catch (IOException e) { System.out.println(e.getMessage()); }
-            }
-        }
-    }
-
-    public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException {
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(filepath));
-            T res = initVal;
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                res = callback.doSomethingWithLine(line, res);
-            }
-            return res;
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            throw e;
-        } finally {
-            if (br != null) {
-                try { br.close(); }
-                catch (IOException e) { System.out.println(e.getMessage()); }
-            }
-        }
-    }
-
-    public String concatenate(String filepath) throws IOException {
-        LineCallback<String> concatenateCallback =
-            new LineCallback<String>() {
-                @Override
-                public String doSomethingWithLine(String line, String value) {
-                    return value + line;
-                }};
-        return lineReadTemplate(filepath, concatenateCallback, "");
-    }
-
-}
+//    public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException {
+//        BufferedReader br = null;
+//        try {
+//            br = new BufferedReader(new FileReader(filepath));
+//            T res = initVal;
+//            String line = null;
+//            while ((line = br.readLine()) != null) {
+//                res = callback.doSomethingWithLine(line, res);
+//            }
+//            return res;
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//            throw e;
+//        } finally {
+//            if (br != null) {
+//                try { br.close(); }
+//                catch (IOException e) { System.out.println(e.getMessage()); }
+//            }
+//        }
+//    }
+//
+//    public String concatenate(String filepath) throws IOException {
+//        LineCallback<String> concatenateCallback =
+//            new LineCallback<String>() {
+//                @Override
+//                public String doSomethingWithLine(String line, String value) {
+//                    return value + line;
+//                }};
+//        return lineReadTemplate(filepath, concatenateCallback, "");
+//    }
+//
+//}
